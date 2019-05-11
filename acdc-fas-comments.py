64b0a84c14b4b7f360767a7262e5b87e40f208b9
courtneyamber <47646034+courtneyamber@users.noreply.github.com>
2019-05-11 18:07:10 -0500

    code w/ comments

--- acdc-fas-comments.py
+++ acdc-fas-comments.py
@@ -43,13 +43,13 @@ def process_collection(collection,filename,filename_struc):
                 replace('Box:', '', regex=True).str.\
                 rstrip(' ;,.').str.lstrip(' ;,.')
 
-    for index,row in df.iterrows():
-        if row["SortKey"].isdigit():
-            row["SortKey"] = row["SortKey"].zfill(25)
-        elif row["SortKey"] == "nan":
-            row["SortKey"] = "".rjust(25, 'Z')
-        else:
-            row["SortKey"] = row["SortKey"].upper()
+    for index,row in df.iterrows():                           # iterating through each cell in the dataframe
+        if row["SortKey"].isdigit():                           # checking to see if the information in the sort key row is a                                                                  digit   
+            row["SortKey"] = row["SortKey"].zfill(25)           # if so, ?????????????????
+        elif row["SortKey"] == "nan":                           # checking to see if the information in the sort key row is                                                                   nothing
+            row["SortKey"] = "".rjust(25, 'Z')                  # if so, ??????????????????????????
+        else:                                                   # checking for anything else
+            row["SortKey"] = row["SortKey"].upper()             # ??????????????????????????
 
     df.sort_values(["SortKey","Folder"], ascending=[True,True],inplace=True)
     df.reset_index(drop=True,inplace=True)
@@ -61,89 +61,89 @@ def process_collection(collection,filename,filename_struc):
 ##     RemainingData  -- remaining data found in box subfield
 ##  only data from the box subfield is used to populate these extra field
 
-    citations = []      # Initializing the list for all of the citations the collection to go in
-    for index,row in df.iterrows():     # looping over every cell in the dataframe 
-        box = row['Box']                # assigning box as what is contained in row "Box"
-        folder = row['Folder']          # assigning folder as what is contained in row "Folder"
-        record_id = row["1"]            # assigning the record_id to what is contained in row "1"
-        record_id = record_id.replace('ACDC_','')      #replacing the beginning of the record_id so it only contains the actua number
-        author = ""                     # initializing the author variable as an empty string
-        for subfield in row['100']:     # 
-            if subfield != "nan":
-                if subfield[0] =="a":
+    citations = []                             # Initializing the list for all of the citations the collection to go in
+    for index,row in df.iterrows():             # looping over every cell in the dataframe 
+        box = row['Box']                        # assigning box as what is contained in row "Box"
+        folder = row['Folder']                   # assigning folder as what is contained in row "Folder"
+        record_id = row["1"]                       # assigning the record_id to what is contained in row "1"
+        record_id = record_id.replace('ACDC_','')      #replacing the beginning of the record_id so it only contains the                                                              actual number
+        author = ""                              # initializing the author variable as an empty string
+        for subfield in row['100']:             # looping over each subfield in row 100 (aka the author information)
+            if subfield != "nan":                 # checking to see if the subfield doesn't equal nothing
+                if subfield[0] =="a":             # if the first index position of that 
                     author = subfield[1:]
-        addauthor = ""
-        if row["700"] != "nan":
-            for subfield in row["700"].split("$"):
-                if subfield != "":
-                    if subfield[0] == "a":
-                        addauthor += subfield[1:] + ", "
-            if addauthor != "":
-                addauthor = addauthor[0:len(addauthor)-2]
-        title = row["245"]
-        title = title.replace('$a','')
-        year = "undated"
-        for subfield in row["260"].split("$"):
-            if subfield != "":
-                if subfield[0] == "c":
-                    if subfield[1:] != "":
-                        year = subfield[1:]
+        addauthor = ""                            # initializing the additional author variable (addauthor) as an empty                                                          string  
+        if row["700"] != "nan":                         # checking to see if row 700 does not equal nothing 
+            for subfield in row["700"].split("$"):      # splitting row 700 on the delimeter and looping through each                                                                 subfield
+                if subfield != "":                         # checking to see if the subfield does not equal an empty string
+                    if subfield[0] == "a":                  # checking to see if the index position of 0 equals an a
+                        addauthor += subfield[1:] + ", "      # if so, adding the rest of that subfield information after                                                                  the a to the addauthor variable along with a comma and space
+            if addauthor != "":                               # checking to see if the addauthor field variable doesn't                                                                       equal nothing
+                addauthor = addauthor[0:len(addauthor)-2]       # ???????????????????????
+        title = row["245"]                                  # assigning the contents of row 245 to the title variable
+        title = title.replace('$a','')                      # replacing the delimeter and subfield a with nothing
+        year = "undated"                                #initalizing the year variable as undated (used for things with no                                                            date information 
+        for subfield in row["260"].split("$"):          # splitting row 260 on the delimeter and looping over each subfield
+            if subfield != "":                           # checking to see if the subfield doesn't equal nothing
+                if subfield[0] == "c":                   # checking to see if the first index position in subfield equals c
+                    if subfield[1:] != "":                  # if the remaining content of subfield c doen't equal nothing...
+                        year = subfield[1:]                 # ... assigning this information to variable year 
 
-        citation = ""
+        citation = ""                   # Initialized each citation to be created as an empty string
 
-        if box != "nan":
-            citation += box + '. '
-        if folder != "nan":
-            citation += folder + '. '
-        citation += record_id
-        if author != "nan":
-            citation += author + ", "
-        if addauthor != "nan":
-            citation += addauthor
-        citation += title +". "
-        citation += "(" + year +")."
+        if box != "nan":                # checking to see if the box variable doesn't equal nothing
+            citation += box + '. '      # if box doesn't equal nothing, adding the contents of box to citation along with a                                           period and space
+        if folder != "nan":             # checking to see if the folder variable doesn't equal nothing
+            citation += folder + '. '   # if folder doesn't equal nothing, adding the contents of folder to citation along                                             with a period and space
+        citation += record_id           # adding the record_id to the ciation 
+        if author != "nan":             # checking to see if author doesn't equal nothing
+            citation += author + ", "   # if author doesn't equal nothing, adding the contents of author to citation, along                                           with a comma
+        if addauthor != "nan":          # checking to see if addauthor doesn't equal nothing
+            citation += addauthor       # if addauthor doesn't equal nothing, adding the contents of addauthor (that is any                                            additional author) to the citation
+        citation += title +". "         # adding the contents of title to the citation along with a period and space
+        citation += "(" + year +")."    # adding the contents of year to the citation, along with encompassing parathesis                                             and a period at the end
 
-        citations.append(citation)
-    outfile = open(filename,"w",encoding="utf-8")
-    outfile2 = open(filename_struc,'w',encoding='utf-8')
+        citations.append(citation)          # adding each citation to the citations list
+    outfile = open(filename,"w",encoding="utf-8")         # opening outfile (aka the raw inventory)
+    outfile2 = open(filename_struc,'w',encoding='utf-8')    # opening outfile2 (aka the structured inventory) 
 
-    lastbox = ""
-    for citation in citations:
-        print(citation, file = outfile)
-        box = citation.split(". ")[0]
-        if lastbox != box:
-            print(box, file = outfile2)
-        print(citation[citation.find(". ")+1:], file = outfile2)
-        lastbox = box
+    lastbox = ""                  # initializing the lastbox variable as an empty string 
+    for citation in citations:        #looping over each citation in the list of citations
+        print(citation, file = outfile)     # printing the citation to the outfile (aka the raw inventory)
+        box = citation.split(". ")[0]       # splitting each citation on a period-space, taking the first index position of                                               that new string, and assigning it to the variable "box"
+        if lastbox != box:                  # looking to see if the lastbox does not equal box
+            print(box, file = outfile2)     # printing out box to outfile2 (aka the structured inventory)
+        print(citation[citation.find(". ")+1:], file = outfile2)    #finding the portion of ciation that follows the box                                                                          information and printing that out to outfile2 (aka the                                                                        structured inventory) beneath the box information 
+        lastbox = box                      # assigning lastbox as box to keep track of the last box number in the list that                                               way each citation with the same box is grouped together beneath that box                                                      statement
 
-    outfile.close()
+    outfile.close()       #closing both files
     outfile2.close()
     
-def inventories():
-    for collection in collections:
-        process_collection(collection, collection.replace(" ","-") + "-inventory.txt", collection.replace(" ","-")+ "-structured-inventory.txt")
+def inventories():      #defining a function that will run each collection to the process collection function 
+    for collection in collections:    #iterating through each collection in the collections list
+        process_collection(collection, collection.replace(" ","-") + "-inventory.txt", collection.replace(" ","-")+ "-structured-inventory.txt")            # adding parameters into the process collection function
 
 def process_findingaid(collection):       # defining a function that processes each of the 
     infile = open('fa_template.txt','r',encoding='utf-8')     # opening the finding aid template
     template = infile.read()                # reading in the finding aid template     
     infile.close()                          # closing the finding aid template file 
 
-    infile2 = open(collection.replace(" ","-")+ "-structured-inventory.txt",'r',encoding='utf-8')
-    inventory = infile2.read()
+    infile2 = open(collection.replace(" ","-")+ "-structured-inventory.txt",'r',encoding='utf-8')   #reading in the                                                                                                             structured inventory to                                                                                                       be added to the                                                                                                             finding aid
+    inventory = infile2.read()    
     infile2.close()
 
     findingaid = template
-    findingaid = findingaid.replace("[@[*? title ?*]@]",collection + " Collection")     # replacing the title element in the template with the title for each collection
-    findingaid = findingaid.replace("[@[*? creator ?*]@]","Creator: " + collection)     # replacing the creator element in the template with the creator for each collection
-    findingaid = findingaid.replace("[@[*? structured inventory ?*]@]",inventory)        # replacing the structured inventory element in the template with the inventory for each collection
+    findingaid = findingaid.replace("[@[*? title ?*]@]",collection + " Collection")     # replacing the title element in the                                                                                          template with the title for each                                                                                              collection
+    findingaid = findingaid.replace("[@[*? creator ?*]@]","Creator: " + collection)     # replacing the creator element in                                                                                              the template with the creator for                                                                                             each collection
+    findingaid = findingaid.replace("[@[*? structured inventory ?*]@]",inventory)        # replacing the structured                                                                                                   inventory element in the                                                                                                      template with the inventory for                                                                                               each collection
 
-    outfile = open(collection.replace(" ","-") + "-findingaid.md",'w',encoding='utf-8')
+    outfile = open(collection.replace(" ","-") + "-findingaid.md",'w',encoding='utf-8')   # writing out each finding aid as                                                                                             a .md file
     print(findingaid, file = outfile)
     outfile.close()
 
 def findingaids():                        #defining a function to generate finding aids for each collection
     for collection in collections:              # looping through each collection in the list of collections 
-        process_findingaid(collection)           # calling on the process finding aids function and using each collection as a parameter
+        process_findingaid(collection)           # calling on the process finding aids function and using each collection as                                                     a parameter
     
 inventories()           # calling the inventories function
 findingaids()           # calling the finding aids function
