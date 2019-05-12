3161263791d8d563e6fff247fa30e4dae55fbffa
courtneyamber <47646034+courtneyamber@users.noreply.github.com>
2019-05-12 10:37:04 -0500

    code w/ comments

--- acdc-fas-comments.py
+++ acdc-fas-comments.py
@@ -22,88 +22,109 @@ collections = ['Volume One Number One','AgComm Teaching','Theses','INTERPAK','Os
 
 # Defining a function that processes each collection and outputs them into raw and strcutured inventories. 
 def process_collection(collection,filename,filename_struc):
-    df = pd.read_csv('acdc-records.csv')    #reading in the csv file into a dataframe
-    df = df.applymap(str)   #making everything in the dataframe a string
-    df = df[df['500'].str.contains(collection)]     #making the dataframe only contain rows where the 500 note contains a certain collection note
+    df = pd.read_csv('acdc-records.csv')              #reading in the csv file into a dataframe
+    df = df.applymap(str)                               #making everything in the dataframe a string
+    df = df[df['500'].str.contains(collection)]     #making the dataframe only contain rows where the 500 note contains a                                                     certain collection note
 
-    df['Box'] = df['852'].str.\
-                   extract(r'.*\$.*(?P<BoxFolder>Box:.*)').astype(str).BoxFolder.str.\
+    # creating a new Box column in the dataframe
+        # Finding the substring of Box: where it is after the delimeter (aka the "$") and assigns it to BoxFolder.
+        # Truncates any subfield that it finds preceding the subfield Box: was found in.
+        # Getting rid of double-spaces
+        # Replacing the Folder data that it finds in BoxFolder (this will be extracted separately); stripping off trailing                characters
+    
+    df['Box'] = df['852'].str.\                                                         
+                   extract(r'.*\$.*(?P<BoxFolder>Box:.*)').astype(str).BoxFolder.str.\  
                    replace('\$.*', '', regex=True).str.\
                    replace('  ', ' ', regex=True).str.\
                    replace('Folder.*', '', regex=True).str.rstrip(' ;,')
+    
+    # Creating a new Folder column in the dataframe
+        # Same as above
+        # Same as above
+        # Same as above
+        # Searches for "Folder:" and assigns it to the variable of Folder; strips trailing characters
+    
+    
     df['Folder'] = df['852'].str.\
                       extract(r'.*\$.*(?P<BoxFolder>Box:.*)').astype(str).BoxFolder.str.\
                       replace('\$.*', '', regex=True).str.\
                       replace('  ', ' ', regex=True).str.\
                       extract(r'.*(?P<Folder>Folder:.*)').astype(str).Folder.str.rstrip(' ;,')
+    
+    # Creating a Plus indicator column in the dataframe.  This is used in the record to indicate if the ACDC physically           holds the document.
+          # Searching for a match of "+" when preceded by delimeter, location information, and followed by Box:
+    
+   
     df['PlusIndictor'] = df['852'].str.\
                             extract(r'.*\$[a-z](?P<PlusIndictor>\+).*Box:.*').astype(str).PlusIndictor
 
+    # Creating a SortKey column in the dataframe to sort the rows by Box. 
+         # starts off with the data in Box, then replacing the "Box:" with nothing, to just get the number
+         # strips off the spaces and punctuation
+    
     df['SortKey'] = df['Box'].str.\
                 replace('Box:', '', regex=True).str.\
                 rstrip(' ;,.').str.lstrip(' ;,.')
+    
+    
+    
 
     for index,row in df.iterrows():                           # iterating through each cell in the dataframe
         if row["SortKey"].isdigit():                           # checking to see if the information in the sort key row is a                                                                  digit   
-            row["SortKey"] = row["SortKey"].zfill(25)           # if so, ?????????????????
+            row["SortKey"] = row["SortKey"].zfill(25)           # if so, it is zero-padding numbers so they sort correctly                                                                      with an alpha-sort (because everything is a string!) 
         elif row["SortKey"] == "nan":                           # checking to see if the information in the sort key row is                                                                   nothing
-            row["SortKey"] = "".rjust(25, 'Z')                  # if so, ??????????????????????????
+            row["SortKey"] = "".rjust(25, 'Z')                  # if so, reassign the row and replace with all "Z"'s so it                                                                      shows up at the end in the apha-sort
         else:                                                   # checking for anything else
-            row["SortKey"] = row["SortKey"].upper()             # ??????????????????????????
+            row["SortKey"] = row["SortKey"].upper()             # Shoving everything into uppercase to ignore character case                                                                    when sorting
 
-    df.sort_values(["SortKey","Folder"], ascending=[True,True],inplace=True)
-    df.reset_index(drop=True,inplace=True)
-    df.drop('SortKey', axis=1,inplace=True)
+    df.sort_values(["SortKey","Folder"], ascending=[True,True],inplace=True)      #sorting the dataframe by the SortKey and                                                                                     Folder columns
+    df.reset_index(drop=True,inplace=True)                                        # Resetting the index, dropping the old                                                                                       one
+    df.drop('SortKey', axis=1,inplace=True)                                       # Dropping the SortKey column because it                                                                                        is no longer needed
 
-## DataFrame now contains the fields Box, Folder, Subfield, PlusIndictor, RemainingData
-##     Subfield -- subfield letter that contained the box data
-##     PlusIndictor -- plus sign in found proceeding subfield letter
-##     RemainingData  -- remaining data found in box subfield
-##  only data from the box subfield is used to populate these extra field
-
-    citations = []                             # Initializing the list for all of the citations the collection to go in
+    
+    citations = []                             # Initializing the list for all of the citations for each collection
     for index,row in df.iterrows():             # looping over every cell in the dataframe 
         box = row['Box']                        # assigning box as what is contained in row "Box"
         folder = row['Folder']                   # assigning folder as what is contained in row "Folder"
         record_id = row["1"]                       # assigning the record_id to what is contained in row "1"
         record_id = record_id.replace('ACDC_','')      #replacing the beginning of the record_id so it only contains the                                                              actual number
         author = ""                              # initializing the author variable as an empty string
-        for subfield in row['100']:             # looping over each subfield in row 100 (aka the author information)
-            if subfield != "nan":                 # checking to see if the subfield doesn't equal nothing
-                if subfield[0] =="a":             # if the first index position of that 
-                    author = subfield[1:]
+        if row["100"] != "nan":                       # checking to see if row 100 doesn't equal nothing
+            for subfield in row['100'].split("$"):    # looping over each subfield in row 100 (aka the author information)
+                if subfield[0] == "a":                   # if the first index position of that subfield is "a"...
+                    author = subfield[1:]                 #.... assigning author as that subfield from index position 1 on
+                    
         addauthor = ""                            # initializing the additional author variable (addauthor) as an empty                                                          string  
         if row["700"] != "nan":                         # checking to see if row 700 does not equal nothing 
             for subfield in row["700"].split("$"):      # splitting row 700 on the delimeter and looping through each                                                                 subfield
-                if subfield != "":                         # checking to see if the subfield does not equal an empty string
-                    if subfield[0] == "a":                  # checking to see if the index position of 0 equals an a
-                        addauthor += subfield[1:] + ", "      # if so, adding the rest of that subfield information after                                                                  the a to the addauthor variable along with a comma and space
-            if addauthor != "":                               # checking to see if the addauthor field variable doesn't                                                                       equal nothing
-                addauthor = addauthor[0:len(addauthor)-2]       # ???????????????????????
+                if subfield[0] == "a":                  # checking to see if the index position of 0 equals an "a"
+                    addauthor += subfield[1:] + ", "      # if so, adding the rest of that subfield information after                                                                  the "a" to the addauthor variable along with a comma and space
+            if addauthor != "":                               # checking to see if the addauthor variable doesn't                                                                       equal nothing
+                addauthor = addauthor[0:len(addauthor)-2]       # stripping off the last comma space from the last addauthor                                                                    value in the addauthor variable
         title = row["245"]                                  # assigning the contents of row 245 to the title variable
         title = title.replace('$a','')                      # replacing the delimeter and subfield a with nothing
         year = "undated"                                #initalizing the year variable as undated (used for things with no                                                            date information 
-        for subfield in row["260"].split("$"):          # splitting row 260 on the delimeter and looping over each subfield
-            if subfield != "":                           # checking to see if the subfield doesn't equal nothing
-                if subfield[0] == "c":                   # checking to see if the first index position in subfield equals c
-                    if subfield[1:] != "":                  # if the remaining content of subfield c doen't equal nothing...
-                        year = subfield[1:]                 # ... assigning this information to variable year 
+        if row["260"] != "nan":                         #checking to see if row 260 doesn't equal nothing
+            for subfield in row["260"].split("$"):       # splitting row 260 on the delimeter and looping over each subfield
+                if subfield[0] == "c":               # checking to see if the first index position in subfield equals a "c"
+                    year = subfield[1:]            # ... assigning this information to variable year 
 
-        citation = ""                   # Initialized each citation to be created as an empty string
+        citation = ""                   # Initializing each citation to be created as an empty string
 
         if box != "nan":                # checking to see if the box variable doesn't equal nothing
             citation += box + '. '      # if box doesn't equal nothing, adding the contents of box to citation along with a                                           period and space
         if folder != "nan":             # checking to see if the folder variable doesn't equal nothing
             citation += folder + '. '   # if folder doesn't equal nothing, adding the contents of folder to citation along                                             with a period and space
         citation += record_id           # adding the record_id to the ciation 
-        if author != "nan":             # checking to see if author doesn't equal nothing
-            citation += author + ", "   # if author doesn't equal nothing, adding the contents of author to citation, along                                           with a comma
-        if addauthor != "nan":          # checking to see if addauthor doesn't equal nothing
-            citation += addauthor       # if addauthor doesn't equal nothing, adding the contents of addauthor (that is any                                            additional author) to the citation
-        citation += title +". "         # adding the contents of title to the citation along with a period and space
-        citation += "(" + year +")."    # adding the contents of year to the citation, along with encompassing parathesis                                             and a period at the end
+        if author != "":                      # checking to see if author doesn't equal an empty space
+            citation += author + ", "   # if author doesn't equal an empty space, adding the contents of author to citation,                                              along with a comma and space
+        if addauthor != "":                   # checking to see if addauthor doesn't equal nothing
+            citation += addauthor + ", "       # if addauthor doesn't equal nothing, adding the contents of addauthor (that                                                     is any additional author) to the citation
+        citation += '"' + title + '". '     # adding the contents of title to the citation surround by quotes along with a                                                    period and space
+        citation += "(" + year + ")."    # adding the contents of year to the citation, along with encompassing parathesis                                             and a period at the end
 
-        citations.append(citation)          # adding each citation to the citations list
+        citations.append(citation)                    # adding each citation to the citations list
+        
     outfile = open(filename,"w",encoding="utf-8")         # opening outfile (aka the raw inventory)
     outfile2 = open(filename_struc,'w',encoding='utf-8')    # opening outfile2 (aka the structured inventory) 
 
@@ -121,9 +142,9 @@ def process_collection(collection,filename,filename_struc):
     
 def inventories():      #defining a function that will run each collection to the process collection function 
     for collection in collections:    #iterating through each collection in the collections list
-        process_collection(collection, collection.replace(" ","-") + "-inventory.txt", collection.replace(" ","-")+ "-structured-inventory.txt")            # adding parameters into the process collection function
+        process_collection(collection, collection.replace(" ","-") + "-raw-inventory.txt", collection.replace(" ","-")+ "-structured-inventory.txt")            # calling the process_collection function, passing in the collection name and                                                 filenames to be used for the output 
 
-def process_findingaid(collection):       # defining a function that processes each of the 
+def process_findingaid(collection):                    # defining a function that processes each of the collections into                                                                    finding aids 
     infile = open('fa_template.txt','r',encoding='utf-8')     # opening the finding aid template
     template = infile.read()                # reading in the finding aid template     
     infile.close()                          # closing the finding aid template file 
@@ -132,7 +153,7 @@ def process_findingaid(collection):       # defining a function that processes e
     inventory = infile2.read()    
     infile2.close()
 
-    findingaid = template
+    findingaid = template                                                               # initializing the finding aid with                                                                                           the finding aid template
     findingaid = findingaid.replace("[@[*? title ?*]@]",collection + " Collection")     # replacing the title element in the                                                                                          template with the title for each                                                                                              collection
     findingaid = findingaid.replace("[@[*? creator ?*]@]","Creator: " + collection)     # replacing the creator element in                                                                                              the template with the creator for                                                                                             each collection
     findingaid = findingaid.replace("[@[*? structured inventory ?*]@]",inventory)        # replacing the structured                                                                                                   inventory element in the                                                                                                      template with the inventory for                                                                                               each collection
